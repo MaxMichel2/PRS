65aab1462a45e4144cfe537dcddd8189bcdb3e05
Maxime <maxime_michel_uk@hotmail.com>
2019-12-02 08:40:39 +0100

    Project start

---
+++ Projet/bin/serveur1-H4M4C.c
@@ -0,0 +1,18 @@
+//Librairies_________________________________________________________________________________________________________________________________________________________________________________________
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <math.h>
